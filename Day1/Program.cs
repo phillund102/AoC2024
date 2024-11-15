2f33d39849e8af22a95705b1e4ca1cef38a9671c
Philip Lund <philip.lund@prevas.se>
2024-11-15 08:55:27 +0100

    Add project files.

---
+++ Day1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Day1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var readText = File.ReadAllText("input.txt").ToArray();
+
+            Part1(readText);
+        }
+
+        public static void Part1(char[] input)
+        {
+
+        }
+    }
+}
